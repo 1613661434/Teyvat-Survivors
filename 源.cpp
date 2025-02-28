b9f98f08be2ad2262f31d9a43f08fb6764758a0f
OL <1613661434@qq.com>
2025-03-01 00:54:12 +0800

    击杀敌人bug

--- 源.cpp
+++ 源.cpp
@@ -158,9 +158,19 @@ public:
 		else { putimage_alpha(player_pos.x, player_pos.y, frame_list_right[idx_frame]); }
 	}
 
-	POINT GetPosition() const
+	const POINT& GetPosition() const
 	{
 		return player_pos;
+	}
+
+	const int GetPlayerWidth() const
+	{
+		return PLAYER_WIDTH;
+	}
+
+	const int GetPlayerHeight() const
+	{
+		return PLAYER_HEIGHT;
 	}
 };
 
@@ -234,14 +244,24 @@ public:
 
 	~Enemy() = default;
 
+	//检测敌人与子弹碰撞
 	bool CheckBulletCollision(const Bullet& bullet)
 	{
-		return false;
+		//将子弹等效为点，判断点是否在敌人矩形内
+		bool is_overlap_x = enemy_pos.x <= bullet.position.x && bullet.position.x <= enemy_pos.x + ENEMY_WIDTH;
+		bool is_overlap_y = enemy_pos.y <= bullet.position.y && bullet.position.y <= enemy_pos.y + ENEMY_HEIGHT;
+		return is_overlap_x && is_overlap_y;
 	}
 
+	//检测敌人与玩家碰撞
 	bool CheckPlayerCollision(const Player& player)
 	{
-		return false;
+		//将敌人中心位置等效为点，判断点是否在玩家矩形内
+		POINT check_position = { enemy_pos.x + ENEMY_WIDTH / 2,enemy_pos.y + ENEMY_HEIGHT / 2 };
+		POINT player_pos = player.GetPosition();
+		bool is_overlap_x = player_pos.x <= check_position.x && check_position.x <= player_pos.x + ENEMY_WIDTH;
+		bool is_overlap_y = player_pos.y <= check_position.y && check_position.y <= player_pos.y + ENEMY_HEIGHT;
+		return is_overlap_x && is_overlap_y;
 	}
 
 	void Move(const Player& player)
@@ -289,37 +309,96 @@ public:
 	}
 };
 
+//更新子弹位置
+void UpdateBullets(std::vector<Bullet>& bullet_list, const Player& player)
+{
+#define RADIAL_SPEED 0.0045 //径向波动速度
+#define TANGENT_SPEED 0.0055 //切向波动速度
+	double radian_interval = 2 * 3.14159 / bullet_list.size(); //子弹之间的弧度间隔
+	POINT player_position = player.GetPosition();
+	double radius = 100 + 25 * sin(clock() * RADIAL_SPEED);
+	for (size_t i = 0; i < bullet_list.size(); ++i)
+	{
+		double radian = clock() * TANGENT_SPEED + radian_interval * i; //当前子弹所在的弧度值
+		bullet_list[i].position.x = player_position.x + player.GetPlayerWidth() / 2 + (int)(radius * sin(radian));
+		bullet_list[i].position.y = player_position.y + player.GetPlayerHeight() / 2 + (int)(radius * cos(radian));
+	}
+}
+
 int main()
 {
 	initgraph(WINDOW_WIDTH, WINDOW_HEIGHT);
 
 	bool running = true;
 
+	//加载背景
 	IMAGE img_background;
 	loadimage(&img_background, _T("img/background.png"));
 
+	//角色类实例
 	Player anim(_T("img/player_left_%d.png"), _T("img/player_right_%d.png"), _T("img/shadow_player.png"), 6, 29);
-	std::vector<Enemy*>enemy_list;
+
+	//子弹类线性表
+	std::vector<Bullet> bullet_list(3);
+
+	//敌人类指针线性表
+	std::vector<Enemy*> enemy_list;
 	BeginBatchDraw();
 
 	while (running)
 	{
 		int start_time = clock();
 
+		//角色移动
 		anim.Move();
+
+		//更新子弹位置
+		UpdateBullets(bullet_list, anim);
+
+		//敌人生成
 		Enemy::TryGenerateEnemy(enemy_list);
 		for (Enemy* enemy : enemy_list)
 		{
 			enemy->Move(anim);
 		}
 
+		//检测敌人与玩家的碰撞
+		for (size_t i = 0; i < enemy_list.size(); ++i)
+		{
+			Enemy* enemy = enemy_list[i];
+			if (enemy->CheckPlayerCollision(anim))
+			{
+				MessageBox(GetHWnd(), _T("扣“1”观看战绩CG"), _T("游戏结束"), MB_OK);
+				running = false;
+				break;
+			}
+			//检测敌人与子弹的碰撞，是则删除敌人
+			for (const Bullet& bullet : bullet_list)
+			{
+				if (enemy->CheckBulletCollision(bullet))
+				{
+					std::swap(enemy, enemy_list.back());
+					enemy_list.pop_back();
+					delete enemy;
+				}
+			}
+		}
+
 		cleardevice();
 
+		//绘画背景
 		putimage(0, 0, &img_background);
+		//绘画角色
 		anim.Draw();
+		//绘画敌人
 		for (Enemy* enemy : enemy_list)
 		{
 			enemy->Draw();
+		}
+		//绘画子弹
+		for (const Bullet& bullet : bullet_list)
+		{
+			bullet.Draw();
 		}
 
 		FlushBatchDraw();
